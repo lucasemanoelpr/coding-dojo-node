beb696d51112010a7ed6a50234b11597fbbabb70
Lucas Fonseca <lucas@boxti.com.br>
2022-01-11 11:22:24 -0300

    first commit :D

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	roots: ['<rootDir>/tests'],
+	collectCoverageFrom: ['<rootDir>/tests/**/*.ts'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.ts$': 'ts-jest'
+	}
+}
